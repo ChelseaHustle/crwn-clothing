c2a4335d802ed032cf06829982bafec67ff1f8b0
ChelseaHustle <chelsea_ok2000@hotmail.com>
2020-12-23 21:01:50 +0000

    Added redux and implemented userReducer and userActions to header and components

---
+++ src/redux.js
@@ -0,0 +1,13 @@
+//blueprint for a reducer
+
+const userReducer = (currentState, action) => {
+    switch(action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...currentState,
+                currentUser: action.payload
+            };
+            default:
+                return currentState;
+    }
+}; // => { currentUser:{...}}
